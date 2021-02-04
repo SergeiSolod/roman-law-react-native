e0539f39d44a09c66e0f4fec909f085204100de1
SergeySolod <solodsergey287@gmail.com>
2021-02-05 00:50:35 +0300

    Commit Message

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,8 @@
 export const THEME = {
-    MAIN_COLOR: 'green',
+    MAIN_COLOR: '#cc8400',
+    FIRST_BUTTON_COLOR: '#ffa500',
+    SECOND_BUTTON_COLOR: '#e69e19',
+    THIRD_BUTTON_COLOR: '#ed9121',
+    FOURTH_BUTTON_COLOR: '#ffba00',
     DANGER_COLOR: 'red'
 }
