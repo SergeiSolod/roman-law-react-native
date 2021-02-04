f9dd03149570856014a6c1a33db32208b6bdac8a
SergeySolod <solodsergey287@gmail.com>
2021-02-05 00:50:20 +0300

    Commit Message

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -4,11 +4,13 @@ import {Platform} from 'react-native'
 import Home from "../screens/Home/Home";
 import Task1 from "../screens/Tasks/Task1";
 import {THEME} from '../theme'
+import Task2 from "../screens/Tasks/Task2";
 
 const PostNavigator = createStackNavigator(
     {
         Home: Home,
         Task1: Task1,
+        Task2: Task2,
     },
     {
         initialRouteName: 'Home',
