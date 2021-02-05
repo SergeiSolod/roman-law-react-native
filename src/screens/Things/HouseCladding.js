aa5ad84af9f46fe9a570500b7db85cbbf086606b
SergeySolod <solodsergey287@gmail.com>
2021-02-05 19:53:29 +0300

    Commit Message

--- src/screens/Things/HouseCladding.js
+++ src/screens/Things/HouseCladding.js
@@ -1,89 +1,94 @@
 import React, {useState} from "react";
 import {View, Text, StyleSheet, Button,SafeAreaView, ScrollView, TouchableOpacity} from 'react-native'
 import { THEME } from '../../theme'
+import CButton from "../../components/CButton";
 
 const HouseCladding = () => {
-    const [answer, changeAnswer] = useState(false);
-    const [answerBool, changeAnswerBool] = useState(false);
-    const [openInfo, changeOpenInfo] = useState(false);
-    const [openHelp, changeOpenHelp] = useState(false);
+    const [openTheory, changeOpenTheory] = useState(false);
 
-    function trueAnswerFunction() {
-        changeAnswer(true)
-        changeAnswerBool(true)
-    }
-
-    function falseAnswerFunction() {
-        changeAnswer(false)
-        changeAnswerBool(false)
-    }
-
-    function openInfoFunction() {
-        if (openInfo) {
-            changeOpenInfo(false)
-        } else {
-            changeOpenInfo(true)
-        }
-    }
-
-    function openHelpFunction() {
-        if (openHelp) {
-            changeOpenHelp(false)
-        } else {
-            changeOpenHelp(true)
-        }
-    }
+    // const [answer, changeAnswer] = useState(false);
+    // const [answerBool, changeAnswerBool] = useState(false);
+    //
+    // const [openHelp, changeOpenHelp] = useState(false);
+    //
+    // function trueAnswerFunction() {
+    //     changeAnswer(true)
+    //     changeAnswerBool(true)
+    // }
+    //
+    // function falseAnswerFunction() {
+    //     changeAnswer(false)
+    //     changeAnswerBool(false)
+    // }
+    //
+    // function openInfoFunction() {
+    //     if (openInfo) {
+    //         changeOpenInfo(false)
+    //     } else {
+    //         changeOpenInfo(true)
+    //     }
+    // }
+    //
+    // function openHelpFunction() {
+    //     if (openHelp) {
+    //         changeOpenHelp(false)
+    //     } else {
+    //         changeOpenHelp(true)
+    //     }
+    // }
 
     return (
         <SafeAreaView>
             <ScrollView>
-        <View style={styles.top}>
+        <View style={styles.textTaskWrapper}>
             <Text style={styles.textTask}>
                 Зей, передавая Гаю дом во исполнение купли-продажи, приказал снять дорогую мраморную облицовку полов,
                 мотивируя тем, что при заключении договора речь шла о доме, а об облицовке дома ничего не сказано.
-                <Text style={styles.textQuestion}>
-                    Что должен решить претор?
-                </Text>
             </Text>
+            <Text style={styles.textQuestion}>
+                Как следует решить дело?
+            </Text>
+        </View>
+            {/*{!answer &&*/}
+            {/*<View>*/}
+            {/*    <Button  title='Зей прав, снимаем облицовку' onPress={trueAnswerFunction}/>*/}
+            {/*<View style={styles.secondButton}>*/}
+            {/*<Button  title='Гай прав, облицовку не снимаем' onPress={falseAnswerFunction}/>*/}
+            {/*</View>*/}
+            {/*</View>*/}
+            {/*}*/}
 
-            {!answer &&
-            <View>
-                <Button  title='Зей прав, снимаем облицовку' onPress={trueAnswerFunction}/>
-            <View style={styles.secondButton}>
-            <Button  title='Гай прав, облицовку не снимаем' onPress={falseAnswerFunction}/>
-            </View>
-            </View>
-            }
+            {/*{answer && <View style={styles.center}>*/}
+            {/*    {answerBool ? <Text style={styles.textInfo}>*/}
+            {/*        Решение правильное, молодец. Если ты прочитал теорию, то сразу понял, что дом - это собирательная вещь,*/}
+            {/*         облицовка - это часть дома, если продаётся дом, то продаётся и облицовка в том числе, потому что это часть дома,*/}
+            {/*        это и есть по сути дом, поэтому Гай конечно же прав. То же самое, если бы Зей сказал, что не передаст крышу*/}
+            {/*        или стену, она видите ли дорогая. Другое дело, если бы речь шла, например, о сундуке. Сундук - вещь самостоятельная, отдельная,*/}
+            {/*        он не является частью дома и его продают отдельно, его требовать нельзя.*/}
+            {/*    </Text> : <Text style={styles.textInfo}>*/}
+            {/*        Решение неправильное... Если ты прочитал теорию, то сразу понял, что дом - это собирательная вещь,*/}
+            {/*        облицовка - это часть дома, если продаётся дом, то продаётся и облицовка в том числе, потому что это часть дома,*/}
+            {/*        это и есть по сути дом, поэтому Гай конечно же прав. То же самое, если бы Зей сказал, что не передаст крышу*/}
+            {/*        или стену, она видите ли дорогая. Другое дело, если бы речь шла, например, о сундуке. Сундук - вещь самостоятельная, отдельная,*/}
+            {/*        он не является частью дома и его продают отдельно, его требовать нельзя.*/}
+            {/*    </Text>}*/}
 
-            {answer && <View style={styles.center}>
-                {answerBool ? <Text style={styles.textInfo}>
-                    Решение правильное, молодец. Если ты прочитал теорию, то сразу понял, что дом - это собирательная вещь,
-                     облицовка - это часть дома, если продаётся дом, то продаётся и облицовка в том числе, потому что это часть дома,
-                    это и есть по сути дом, поэтому Гай конечно же прав. То же самое, если бы Зей сказал, что не передаст крышу
-                    или стену, она видите ли дорогая. Другое дело, если бы речь шла, например, о сундуке. Сундук - вещь самостоятельная, отдельная,
-                    он не является частью дома и его продают отдельно, его требовать нельзя.
-                </Text> : <Text style={styles.textInfo}>
-                    Решение неправильное... Если ты прочитал теорию, то сразу понял, что дом - это собирательная вещь,
-                    облицовка - это часть дома, если продаётся дом, то продаётся и облицовка в том числе, потому что это часть дома,
-                    это и есть по сути дом, поэтому Гай конечно же прав. То же самое, если бы Зей сказал, что не передаст крышу
-                    или стену, она видите ли дорогая. Другое дело, если бы речь шла, например, о сундуке. Сундук - вещь самостоятельная, отдельная,
-                    он не является частью дома и его продают отдельно, его требовать нельзя.
-                </Text>}
+            {/*</View>}*/}
+            {/*<View style={styles.thirdButton}>*/}
+            {/*</View>*/}
+            {/*{openHelp && <View style={styles.textInfoWrapper}>*/}
+            {/*    <Text style={styles.textInfo}>*/}
+            {/*       Я купил машину, должны ли мне передать колёса от машины? Ответ очевиден - да*/}
+            {/*    </Text>*/}
+            {/*</View>}*/}
 
-            </View>}
-            <View style={styles.thirdButton}>
-            <Button  title={openHelp ? 'Закрыть подсказку' : 'Посмотреть подсказку'} onPress={openHelpFunction}/>
-            </View>
-            {openHelp && <View style={styles.textInfoWrapper}>
-                <Text style={styles.textInfo}>
-                   Я купил машину, должны ли мне передать колёса от машины? Ответ очевиден - да
-                </Text>
-            </View>}
-
-            <Button  title={openInfo ? 'Закрыть теорию' : 'Посмотреть теорию'} onPress={openInfoFunction}/>
+            {openTheory ?   <CButton onPress={() =>  changeOpenTheory(false)} title='Закрыть теорию'/>
+            : <CButton onPress={() =>  changeOpenTheory(true)} title='Открыть теорию'/> }
 
 
-            {openInfo && <View style={styles.textInfoWrapper}>
+
+
+            {openTheory && <View style={styles.textInfoWrapper}>
                 <Text style={styles.textInfo}>
                     В римском праве вещи бывают простые, сложные и составные.
                 </Text>
@@ -106,7 +111,6 @@ const HouseCladding = () => {
                     земельный участок, все постройки на нем)
                 </Text>
             </View>}
-        </View>
             </ScrollView>
         </SafeAreaView>
     )
@@ -117,24 +121,26 @@ HouseCladding.navigationOptions = {
 }
 
 const styles = StyleSheet.create({
-    top: {
+    textTaskWrapper: {
+        marginRight: 10,
+        marginLeft: 10,
+        marginTop: 7,
+        marginBottom: 7,
         flex: 1,
     },
-    secondButton: {
-        marginTop: 5,
-        marginBottom: 5,
+    textTask: {
+        fontSize: 20,
+        textAlign: 'center'
     },
-    buttonInfo: {
-        marginLeft: 10,
-        marginRight: 10,
+    textQuestion: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold'
     },
-    textInfoWrapper: {
 
+    textInfoWrapper: {
         marginRight: 10,
         marginBottom: 5,
-    },
-    thirdButton: {
-      marginBottom: 4
     },
     textInfo: {
         fontSize: 19,
@@ -142,18 +148,9 @@ const styles = StyleSheet.create({
         marginBottom: 8,
         marginLeft: 10,
     },
-    textTask: {
-        fontSize: 20,
-        marginRight: 10,
-        marginLeft: 10,
-        marginTop: 7,
-        marginBottom: 7,
-        flex: 1,
-        textAlign: 'center'
-    },
-    textQuestion: {
+    boldText: {
         fontWeight: 'bold'
-    }
+    },
 })
 
 export default HouseCladding;
