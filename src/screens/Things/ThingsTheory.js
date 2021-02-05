000577da45f31cc8e516506804b7d3d1d174841c
SergeySolod <solodsergey287@gmail.com>
2021-02-05 21:45:41 +0300

    Commit Message

--- src/screens/Things/ThingsTheory.js
+++ src/screens/Things/ThingsTheory.js
@@ -1,15 +1,14 @@
-import React, {useState} from "react";
-import {View, Text, StyleSheet, Button,SafeAreaView, ScrollView, TouchableOpacity} from 'react-native'
-import { THEME } from '../../theme'
-import CButton from "../../components/CButton";
+import React from "react";
+import {View, Text, StyleSheet, SafeAreaView, ScrollView, TouchableOpacity} from 'react-native'
 
 const ThingsTheory = () => {
-
-
     return (
         <SafeAreaView>
             <ScrollView>
                 <View style={styles.textInfoWrapper}>
+                    <Text style={styles.textQuestion}>
+                       Какие бывают вещи
+                    </Text>
                     <Text style={styles.textInfo}>
                         В римском праве вещи бывают простые, сложные и составные.
                     </Text>
@@ -42,33 +41,10 @@ ThingsTheory.navigationOptions = {
 }
 
 const styles = StyleSheet.create({
-    textTaskWrapper: {
-        marginRight: 10,
-        marginLeft: 10,
-        marginTop: 7,
-        marginBottom: 7,
-        flex: 1,
-    },
-    textTask: {
-        fontSize: 20,
-        textAlign: 'center'
-    },
     textQuestion: {
         fontSize: 20,
         textAlign: 'center',
         fontWeight: 'bold'
-    },
-    textInfoCorrectly: {
-        fontSize: 20,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        color: 'green'
-    },
-    textInfoWrong: {
-        fontSize: 20,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        color: 'red'
     },
     textInfoWrapper: {
         marginRight: 10,
