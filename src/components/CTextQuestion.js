98f532bcb9929b60ee510a811e164eb1638edea8
SergeySolod <solodsergey287@gmail.com>
2021-02-06 14:16:24 +0300

    Commit Message

---
+++ src/components/CTextQuestion.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+
+const CTextQuestion = ({ text }) => (
+    <Text style={styles.textQuestion}>
+        {text}
+    </Text>
+);
+
+var styles = StyleSheet.create({
+    textQuestion: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginTop: 6,
+        marginBottom: 8,
+    },
+});
+
+export default CTextQuestion;
