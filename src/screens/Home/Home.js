ed3a2bf60a97480d9374655b9e7c46df94019c11
SergeySolod <solodsergey287@gmail.com>
2021-02-06 15:23:10 +0300

    Commit Message

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -20,51 +20,49 @@ const Home = ({navigation}) => {
             <CButton onPress={() =>  navigation.navigate('CookSlave')} title={'Раб повар'}/>
             <CButton onPress={() =>  navigation.navigate('AppleOrchard')} title={'Яблоневый сад'}/>
             <CButton onPress={() =>  navigation.navigate('LostCrope')} title={'Погибший урожай'}/>
-
-                <Text style={styles.headerText}>
-                    Виндикация
-                </Text>
-                <CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: виндикация'}/>
-
+                {/*<Text style={styles.headerText}>*/}
+                {/*    Виндикация*/}
+                {/*</Text>*/}
+                {/*<CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: виндикация'}/>*/}
             <Text style={styles.headerText}>
                 Подряд
             </Text>
             <CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: подряд'}/>
             <CButton onPress={() =>  navigation.navigate('BurnedOutShip')} title={'Сгоревший корабль'}/>
             <CButton onPress={() =>  navigation.navigate('BurntNecklace')} title={'Сгоревшее ожерелье'}/>
-             <CButton onPress={() =>  navigation.navigate('Sculpture')} title={'Скульптура'}/>
+            <CButton onPress={() =>  navigation.navigate('Sculpture')} title={'Скульптура'}/>
 
 
-                <Text style={styles.headerText}>
-                    Хранение
-                </Text>
-                <CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Хранение'}/>
+                {/*<Text style={styles.headerText}>*/}
+                {/*    Хранение*/}
+                {/*</Text>*/}
+                {/*<CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Хранение'}/>*/}
 
-                <Text style={styles.headerText}>
-                    Легат
-                </Text>
-                <CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Легат'}/>
+                {/*<Text style={styles.headerText}>*/}
+                {/*    Легат*/}
+                {/*</Text>*/}
+                {/*<CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Легат'}/>*/}
 
-                <Text style={styles.headerText}>
-                    Аренда
-                </Text>
-                <CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Аренда'}/>
+                {/*<Text style={styles.headerText}>*/}
+                {/*    Аренда*/}
+                {/*</Text>*/}
+                {/*<CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Аренда'}/>*/}
 
 
-                <Text style={styles.headerText}>
-                    Заём и залог
-                </Text>
-                <CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Заём и залог'}/>
+                {/*<Text style={styles.headerText}>*/}
+                {/*    Заём и залог*/}
+                {/*</Text>*/}
+                {/*<CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Заём и залог'}/>*/}
 
-                <Text style={styles.headerText}>
-                    Деликт
-                </Text>
-                <CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Деликт'}/>
+                {/*<Text style={styles.headerText}>*/}
+                {/*    Деликт*/}
+                {/*</Text>*/}
+                {/*<CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Деликт'}/>*/}
 
-                <Text style={styles.headerText}>
-                    Сервитут
-                </Text>
-                <CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Сервитут'}/>
+                {/*<Text style={styles.headerText}>*/}
+                {/*    Сервитут*/}
+                {/*</Text>*/}
+                {/*<CButton onPress={() =>  navigation.navigate('WorkAgreementTheory')} title={'Теория: Сервитут'}/>*/}
 
             </ScrollView>
         </SafeAreaView>
