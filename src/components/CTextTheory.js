b92ed6c33cccbfa44faf08bae2c006383a892d0d
SergeySolod <solodsergey287@gmail.com>
2021-02-06 14:35:12 +0300

    Commit Message

---
+++ src/components/CTextTheory.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+
+
+const CTextTheory = ({ theory }) => (
+    <View>
+    {theory.map(
+            (text, index) => (
+                <Text key={index} style={styles.textInfo}>
+                    {text}
+                </Text>
+            )
+        )}
+    </View>
+);
+
+var styles = StyleSheet.create({
+    textInfo: {
+        fontSize: 19,
+        marginTop: 6,
+        marginBottom: 13,
+        marginLeft: 10,
+    },
+});
+
+export default CTextTheory;
